e9bb25a55e3685ca15ca6b330fb2054c69873654
jagadish1512 <jagadish.1512@gmail.com>
2019-08-22 16:42:24 +0530

    overloading

---
+++ src/org/cts/test1/mmm.java
@@ -0,0 +1,16 @@
+package org.cts.test1;
+
+public class mmm {
+	
+	
+	private void empId() {
+		System.out.println("12345678");
+	}
+	public static void main(String[] args) {
+		
+		
+		mmm m = new mmm();
+		m.empId();
+	}
+
+}
