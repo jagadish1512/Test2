4fffaf025b1b5806697ebfeb9e96b9cdf9238a4b
jagadish1512 <jagadish.1512@gmail.com>
2019-08-22 16:07:46 +0530

    message

---
+++ src/org/cts/test1/ClassA.java
@@ -0,0 +1,16 @@
+package org.cts.test1;
+
+public class ClassA {
+	
+	private void empId() {
+		System.out.println("1234567");
+	}
+	public static void main(String[] args) {
+		
+		
+		ClassA a= new ClassA();
+		
+		a.empId();
+	}
+
+}
